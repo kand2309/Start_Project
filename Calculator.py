2fc94ebdeabc8c3c2fd083ecb1a2384dd5e55e1c
Leo Gal <leogal2309@gmail.com>
2025-10-18 22:06:04 +0300

    end project

--- Calculator.py
+++ Calculator.py
@@ -1,2 +1,29 @@
-print('GG')
-input("Выйти.")
+print('\t\nДобро пожаловать в простой калькукулятор.')
+print("""\t\nДоступные операции:
+·Сложение (+)
+·Вычетание (-)
+·Умножение (*)
+·Деление (/)
+""")
+
+num_1 = float(input('\nВведите первое число:'))
+operator = input('Введите оператор (+, -, *, /):')
+num_2 = float(input('Введите второе число:'))
+
+if operator == "+":
+    result = num_1 + num_2
+    print(f"{num_1} {operator} {num_2} = {result}" )
+
+elif operator == "-":
+    result = num_1 - num_2
+    print(f"{num_1} {operator} {num_2} = {result}" )
+
+elif operator == "*":
+    result = num_1 * num_2
+    print(f"{num_1} {operator} {num_2} = {result}" )
+    
+elif operator == "/":
+    result = num_1 / num_2
+    print(f"{num_1} {operator} {num_2} = {result}" )
+else:
+    print("Ошибка!")
